9a7515c180c73ccbab9fce7124e49914f88cd763
Arshak Aghakaryan <agakaryan.arshak@gmail.com>
2022-02-13 16:16:52 +0400

    feat: do not enable when the system is not macOS

--- lua/auto-dark-mode/utils.lua
+++ lua/auto-dark-mode/utils.lua
@@ -23,4 +23,15 @@ function M.start_job(cmd, opts)
     return id
 end
 
+---@return 'win'|'darwin'|'linux'
+function M.get_os()
+    if package.config:sub(1, 1) == '\\' then
+        return 'win'
+    elseif (io.popen("uname -s"):read '*a'):match 'Darwin' then
+        return 'darwin'
+    else
+        return 'linux'
+    end
+end
+
 return M
