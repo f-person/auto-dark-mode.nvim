66ab04d8bad0162d628157f0a8b665498e5a15db
winston <hey@winston.sh>
2023-07-13 03:38:10 +0200

    feat: linux support

---
+++ lua/auto-dark-mode/types.lua
@@ -0,0 +1,7 @@
+---@class AutoDarkModeOptions
+-- Optional. If not provided, `vim.api.nvim_set_option_value('background', 'dark')` will be used.
+---@field set_dark_mode nil | fun(): nil
+-- Optional. If not provided, `vim.api.nvim_set_option_value('background', 'light')` will be used.
+---@field set_light_mode nil | fun(): nil
+-- Every `update_interval` milliseconds a theme check will be performed.
+---@field update_interval number?
