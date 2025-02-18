89457afc4732882b882c46ccda3c58675ed8cc95
Nick <nick@trkl.dev>
2025-02-18 21:16:13 +1000

    Fix bad dbus-send check

--- lua/auto-dark-mode/init.lua
+++ lua/auto-dark-mode/init.lua
@@ -95,7 +95,7 @@ local function init()
       if system == "Darwin" then
           query_command = { "defaults", "read", "-g", "AppleInterfaceStyle" }
       elseif system == "Linux" then
-          if vim.fn.executable("dbus-send") then
+          if not vim.fn.executable("dbus-send") then
               error([[
           `dbus-send` is not available. The Linux implementation of
           auto-dark-mode.nvim relies on `dbus-send` being on the `$PATH`.
